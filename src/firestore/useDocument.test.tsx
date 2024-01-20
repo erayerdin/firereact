2cc3fc0d6cabcc28241b4fffbef85c8667464af3
Eray Erdin <eraygezer.94@gmail.com>
2024-01-20 12:18:02 +0300

    add sample component test

--- src/firestore/useDocument.test.tsx
+++ src/firestore/useDocument.test.tsx
@@ -3,6 +3,13 @@
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
+import { render, screen } from "@testing-library/react";
+
 test("2 + 2 equals to 4", () => {
   expect(2 + 2).toBe(4);
+});
+
+test("div exists", async () => {
+  render(<div>foo</div>);
+  await screen.findByText(/foo/i);
 });
