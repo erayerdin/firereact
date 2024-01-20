2ced81323758436da8e76774aa9a3d1bdd46d45b
Eray Erdin <eraygezer.94@gmail.com>
2024-01-20 12:08:34 +0300

    configure and add sample test for vitest

---
+++ src/firestore/useDocument.test.ts
@@ -0,0 +1,8 @@
+// Copyright (c) 2024 Eray Erdin
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+test("2 + 2 equals to 4", () => {
+  expect(2 + 2).toBe(4);
+});
---
+++ vitest.config.ts
@@ -0,0 +1,12 @@
+// Copyright (c) 2024 Eray Erdin
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    globals: true,
+  },
+});
