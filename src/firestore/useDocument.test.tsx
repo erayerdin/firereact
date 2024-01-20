ae9e92d44f8a881f091562cfc87c4244bd6bf168
Eray Erdin <eraygezer.94@gmail.com>
2024-01-20 12:37:54 +0300

    add sample test for hook test

--- src/firestore/useDocument.test.tsx
+++ src/firestore/useDocument.test.tsx
@@ -3,7 +3,11 @@
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
-import { render, screen } from "@testing-library/react";
+import { render, renderHook, screen } from "@testing-library/react";
+
+const useSampleHook = () => {
+  return "foo";
+};
 
 test("2 + 2 equals to 4", () => {
   expect(2 + 2).toBe(4);
@@ -12,4 +16,9 @@ test("2 + 2 equals to 4", () => {
 test("div exists", async () => {
   render(<div>foo</div>);
   await screen.findByText(/foo/i);
+});
+
+test("useSampleHook", () => {
+  const { result } = renderHook(() => useSampleHook());
+  expect(result.current).toBe("foo");
 });
