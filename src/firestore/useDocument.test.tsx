680390835a1506d771a05e670a44da5ff2cb6acc
Eray Erdin <eraygezer.94@gmail.com>
2024-01-21 01:24:54 +0300

    add test for useDocument hook > should initially have no snapshot

--- src/firestore/useDocument.test.tsx
+++ src/firestore/useDocument.test.tsx
@@ -8,18 +8,33 @@ import { deleteDoc, doc, setDoc } from "firebase/firestore";
 import { firestore } from "../firebase";
 import useDocument from "./useDocument";
 
+const docRef = doc(firestore, "profiles", "profile1");
+const docData = { displayName: "Vort the Wise" } as const;
+
 describe("useDocument hook", () => {
   it("should be initially loading state", async () => {
     // setup
-    console.log("setting up");
-    const docRef = doc(firestore, "profiles", "profile1");
     await deleteDoc(docRef);
-    await setDoc(docRef, { displayName: "Vort the Wise" });
+    await setDoc(docRef, docData);
 
     // test
     const { result } = renderHook(() => useDocument({ reference: docRef }));
     const { loading } = result.current;
     expect(loading).toBe(true);
+
+    // teardown
+    await deleteDoc(docRef);
+  });
+
+  it("should initially have no snapshot", async () => {
+    // setup
+    await deleteDoc(docRef);
+    await setDoc(docRef, docData);
+
+    // test
+    const { result } = renderHook(() => useDocument({ reference: docRef }));
+    const { snapshot } = result.current;
+    expect(snapshot).toBeUndefined();
 
     // teardown
     await deleteDoc(docRef);
