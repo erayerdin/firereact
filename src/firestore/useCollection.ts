4298ecf6b59499c0a3078907747eda68146efae5
Eray Erdin <eraygezer.94@gmail.com>
2024-01-21 17:19:12 +0300

    add UseCollection

--- src/firestore/useCollection.ts
+++ src/firestore/useCollection.ts
@@ -3,7 +3,9 @@
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
-import { Query } from "firebase/firestore";
+import { FirebaseError } from "firebase/app";
+import { Query, QuerySnapshot } from "firebase/firestore";
+import { useState } from "react";
 
 type UseCollectionParams = {
   query: Query;
@@ -12,11 +14,23 @@ type UseCollectionParams = {
   };
 };
 
+type UseCollection = {
+  loading: boolean;
+  snapshot?: QuerySnapshot;
+  error?: FirebaseError;
+};
+
 const useCollection = ({
   query,
   options = { listen: false },
-}: UseCollectionParams) => {
+}: UseCollectionParams): UseCollection => {
   const { listen } = options;
+
+  const [loading, setLoading] = useState<boolean>(true);
+  const [snapshot, setSnapshot] = useState<QuerySnapshot | undefined>();
+  const [error, setError] = useState<FirebaseError | undefined>();
+
+  return { loading, snapshot, error };
 };
 
 export default useCollection;
