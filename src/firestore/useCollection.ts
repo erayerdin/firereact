061de95bc344366454f1d8ac5f09a01980310b39
Eray Erdin <eraygezer.94@gmail.com>
2024-01-21 17:15:30 +0300

    add UseCollectionParams

--- src/firestore/useCollection.ts
+++ src/firestore/useCollection.ts
@@ -3,6 +3,20 @@
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
-const useCollection = () => {};
+import { Query } from "firebase/firestore";
+
+type UseCollectionParams = {
+  query: Query;
+  options?: {
+    listen: boolean;
+  };
+};
+
+const useCollection = ({
+  query,
+  options = { listen: false },
+}: UseCollectionParams) => {
+  const { listen } = options;
+};
 
 export default useCollection;
