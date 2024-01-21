63edd0cf914a167a9b65eabe439c06bd09fc6638
Eray Erdin <eraygezer.94@gmail.com>
2024-01-22 02:24:05 +0300

    impl and test initially useAddDocument hook > should return ready

--- src/firestore/useAddDocument.test.ts
+++ src/firestore/useAddDocument.test.ts
@@ -2,3 +2,29 @@
 //
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
+
+import { renderHook } from "@testing-library/react";
+import { collection, deleteDoc, doc } from "firebase/firestore";
+import { firestore } from "../firebase";
+import useAddDocument from "./useAddDocument";
+
+const colRef = collection(firestore, "profiles");
+const docRef = doc(firestore, "profiles", "addDocumentProfile");
+const docData = { displayName: "Add Document" };
+
+describe("initially useAddDocument hook", () => {
+  it("should return ready", async () => {
+    // setup
+    await deleteDoc(docRef);
+
+    // test
+    const { result } = renderHook(() =>
+      useAddDocument({ reference: colRef, data: docData }),
+    );
+    const { state } = result.current;
+    expect(state).toBe("ready");
+
+    // teardown
+    await deleteDoc(docRef);
+  });
+});
--- src/firestore/useAddDocument.ts
+++ src/firestore/useAddDocument.ts
@@ -3,6 +3,27 @@
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
-const useAddDocument = () => {};
+import { CollectionReference } from "firebase/firestore";
+import { useState } from "react";
+
+type UseAddDocumentParams = {
+  reference: CollectionReference;
+  data: unknown;
+};
+
+type UseAddDocumentState = "ready" | "loading" | "done";
+
+type UseAddDocument = {
+  state: UseAddDocumentState;
+};
+
+const useAddDocument = ({
+  reference,
+  data,
+}: UseAddDocumentParams): UseAddDocument => {
+  const [state, setState] = useState<UseAddDocumentState>("ready");
+
+  return { state };
+};
 
 export default useAddDocument;
