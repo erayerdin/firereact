cedfa0eecfb4936e3b2d043d22c5b61d33cef052
Eray Erdin <eraygezer.94@gmail.com>
2024-01-22 23:16:00 +0300

    test and impl useDeleteDocument

--- src/firestore/useDeleteDocument.test.ts
+++ src/firestore/useDeleteDocument.test.ts
@@ -2,3 +2,105 @@
 //
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
+
+import { renderHook } from "@testing-library/react";
+import { deleteDoc, doc, getDoc, setDoc } from "firebase/firestore";
+import sleep from "sleep-sleep";
+import { firestore } from "../firebase";
+import useDeleteDocument from "./useDeleteDocument";
+
+const docRef = doc(firestore, "useDeleteDocument", "doc1");
+
+describe("initially useDeleteDocument hook", () => {
+  it("should return ready", async () => {
+    // setup
+    await deleteDoc(docRef);
+
+    // test
+    const { result } = renderHook(() =>
+      useDeleteDocument({ reference: docRef }),
+    );
+    const { state } = result.current;
+    expect(state).toBe("ready");
+
+    // teardown
+    await deleteDoc(docRef);
+  });
+
+  it("should return no error", async () => {
+    // setup
+    await deleteDoc(docRef);
+
+    // test
+    const { result } = renderHook(() =>
+      useDeleteDocument({ reference: docRef }),
+    );
+    const { error } = result.current;
+    expect(error).toBeUndefined();
+
+    // teardown
+    await deleteDoc(docRef);
+  });
+});
+
+describe("as soon as dispatched, useDeleteDocument hook", () => {
+  // it's too hard to guess how many ms to wait to check loading state
+  it.skip("should return loading", async () => {
+    // setup
+    await deleteDoc(docRef);
+
+    // test
+    const { result } = renderHook(() =>
+      useDeleteDocument({ reference: docRef }),
+    );
+    const { dispatch } = result.current;
+    dispatch();
+    await sleep(30);
+
+    const { state } = result.current;
+    expect(state).toBe("loading");
+
+    // teardown
+    await deleteDoc(docRef);
+  });
+});
+
+describe("after dispatched, useDeleteDocument hook", () => {
+  it("should return done", async () => {
+    // setup
+    await deleteDoc(docRef);
+
+    // test
+    const { result } = renderHook(() =>
+      useDeleteDocument({ reference: docRef }),
+    );
+    const { dispatch } = result.current;
+    await dispatch();
+    await sleep(250);
+
+    const { state } = result.current;
+    expect(state).toBe("done");
+
+    // teardown
+    await deleteDoc(docRef);
+  });
+
+  it("should really delete document", async () => {
+    // setup
+    await setDoc(docRef, { displayName: "Use Delete Document" });
+
+    // test
+    const { result } = renderHook(() =>
+      useDeleteDocument({ reference: docRef }),
+    );
+    const { dispatch } = result.current;
+    await dispatch();
+    await sleep(250);
+
+    const docSnapshot = await getDoc(docRef);
+    expect(docSnapshot.exists()).toBe(false);
+
+    // teardown
+    await deleteDoc(docRef);
+  });
+});
--- src/firestore/useDeleteDocument.ts
+++ src/firestore/useDeleteDocument.ts
@@ -3,6 +3,46 @@
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
-const useDeleteDocument = () => {};
+import { FirebaseError } from "firebase/app";
+import { DocumentReference, deleteDoc } from "firebase/firestore";
+import { useState } from "react";
+
+type UseSetDocumentParams = {
+  reference: DocumentReference;
+};
+
+type UseSetDocumentState = "ready" | "loading" | "done";
+type UseSetDocumentDispatcher = () => Promise<void>;
+
+type UseSetDocument = {
+  state: UseSetDocumentState;
+  dispatch: UseSetDocumentDispatcher;
+  error?: FirebaseError;
+};
+
+const useDeleteDocument = ({
+  reference,
+}: UseSetDocumentParams): UseSetDocument => {
+  const [state, setState] = useState<UseSetDocumentState>("ready");
+  const [error, setError] = useState<FirebaseError | undefined>();
+
+  const dispatch: UseSetDocumentDispatcher = async () => {
+    setState("loading");
+    try {
+      await deleteDoc(reference);
+      setState("done");
+    } catch (e) {
+      if (e instanceof FirebaseError) {
+        setState("ready");
+        setError(e);
+      } else {
+        setState("ready");
+        throw e;
+      }
+    }
+  };
+
+  return { state, dispatch, error };
+};
 
 export default useDeleteDocument;
