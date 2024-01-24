59b0b2654a2e5c3eade3d066b85b87b70ac9ce85
Eray Erdin <eraygezer.94@gmail.com>
2024-01-24 13:12:01 +0300

    test and impl later FirestoreDocument component > should return display name

--- src/firestore/FirestoreDocument.comp.test.tsx
+++ src/firestore/FirestoreDocument.comp.test.tsx
@@ -4,12 +4,51 @@
 // https://opensource.org/licenses/MIT
 
 import { render, screen } from "@testing-library/react";
+import { deleteDoc, doc, setDoc } from "firebase/firestore";
+import sleep from "sleep-sleep";
 import { FirestoreDocument } from ".";
+import { firestore } from "../firebase";
+
+const docRef = doc(firestore, "FirestoreDocument", "doc1");
+const docData = { displayName: "Firestore Document" };
 
 describe("initially FirestoreDocument component", () => {
   it("should return loading", async () => {
-    render(<FirestoreDocument loading={() => <div>Loading...</div>} />);
+    // setup
+    await deleteDoc(docRef);
 
+    // test
+    render(
+      <FirestoreDocument
+        reference={docRef}
+        loading={() => <div>Loading...</div>}
+        done={() => <></>}
+      />,
+    );
     expect(screen.getByText("Loading...").innerHTML).toBe("Loading...");
+  });
+});
+
+describe("later FirestoreDocument component", () => {
+  it("should return display name", async () => {
+    // setup
+    await deleteDoc(docRef);
+    await setDoc(docRef, docData);
+
+    // test
+    render(
+      <FirestoreDocument
+        reference={docRef}
+        loading={() => <div>Loading...</div>}
+        done={(snapshot) => <div>{snapshot.data()?.displayName}</div>}
+      />,
+    );
+    await sleep(250);
+    expect(screen.getByText(docData.displayName).innerHTML).toBe(
+      "Firestore Document",
+    );
+
+    // teardown
+    await deleteDoc(docRef);
   });
 });
--- src/firestore/FirestoreDocument.tsx
+++ src/firestore/FirestoreDocument.tsx
@@ -3,12 +3,26 @@
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
+import { DocumentReference, DocumentSnapshot } from "firebase/firestore";
 import { ReactNode } from "react";
+import { useDocument } from ".";
 
 type FirestoreDocumentProps = {
+  reference: DocumentReference;
   loading: () => ReactNode;
+  done: (snapshot: DocumentSnapshot) => ReactNode;
 };
 
-export const FirestoreDocument = ({ loading }: FirestoreDocumentProps) => {
-  return loading();
+export const FirestoreDocument = ({
+  reference,
+  loading,
+  done,
+}: FirestoreDocumentProps) => {
+  const { loading: processing, snapshot, error } = useDocument({ reference });
+
+  if (error) {
+    throw error;
+  }
+
+  return processing ? loading() : done(snapshot!);
 };
