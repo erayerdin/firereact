70744f14c0298c4eccb23fa2baafe7101b218276
Eray Erdin <eraygezer.94@gmail.com>
2024-01-24 18:14:49 +0300

    add NodeComponent

---
+++ src/types.ts
@@ -0,0 +1,10 @@
+// Copyright (c) 2024 Eray Erdin
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+import { ReactNode } from "react";
+
+export type NodeComponent = {
+  children: ReactNode;
+};
