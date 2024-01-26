8224da4a019af1e88a6a68492563ee281c16a902
Eray Erdin <eraygezer.94@gmail.com>
2024-01-26 14:22:22 +0300

    impl FirebaseAuthProvider

--- src/providers/FirebaseAuthProvider.tsx
+++ src/providers/FirebaseAuthProvider.tsx
@@ -3,6 +3,23 @@
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
-export const FirebaseAuthProvider = () => {
-  return <div>FirebaseAuthProvider</div>;
+import { Auth } from "firebase/auth";
+import { createContext } from "react";
+import { NodeComponent } from "../types";
+
+export const FirebaseAuthContext = createContext<Auth | undefined>(undefined);
+
+type FirebaseAuthProviderProps = {
+  auth: Auth;
+} & NodeComponent;
+
+export const FirebaseAuthProvider = ({
+  auth,
+  children,
+}: FirebaseAuthProviderProps) => {
+  return (
+    <FirebaseAuthContext.Provider value={auth}>
+      {children}
+    </FirebaseAuthContext.Provider>
+  );
 };
