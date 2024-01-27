e88a97403776f416230019d1d7941f9503a1f510
Eray Erdin <eraygezer.94@gmail.com>
2024-01-27 11:30:11 +0300

    init SignOut component

--- src/auth/SignOut.comp.test.tsx
+++ src/auth/SignOut.comp.test.tsx
@@ -2,9 +2,3 @@
 //
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
-
-export * from "./useUser";
-
-export * from "./useSignOut";
-
-export * from "./SignOut";
--- src/auth/SignOut.tsx
+++ src/auth/SignOut.tsx
@@ -3,8 +3,6 @@
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
-export * from "./useUser";
-
-export * from "./useSignOut";
-
-export * from "./SignOut";
+export const SignOut = () => {
+  return <div>SignOut</div>;
+};
