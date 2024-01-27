26b8ddab5fefd891142bc5a4a189df29e55980cb
Eray Erdin <eraygezer.94@gmail.com>
2024-01-27 11:42:55 +0300

    test and impl when state is done, SignOut component > should render done

--- src/auth/SignOut.comp.test.tsx
+++ src/auth/SignOut.comp.test.tsx
@@ -4,11 +4,27 @@
 // https://opensource.org/licenses/MIT
 
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { SignOut } from ".";
+import { auth } from "../firebase";
 
 describe("when state is ready, SignOut component", () => {
   it("should render a button", async () => {
-    render(<SignOut onReady={() => <button>Sign Out</button>} />);
+    render(<SignOut auth={auth} onReady={() => <button>Sign Out</button>} />);
     expect(screen.getByText("Sign Out")).not.toBeUndefined();
+  });
+});
+
+describe("when state is done, SignOut component", () => {
+  it("should render done", async () => {
+    render(
+      <SignOut
+        auth={auth}
+        onReady={() => <button>Sign Out</button>}
+        onDone={() => <div>Authed</div>}
+      />,
+    );
+    await userEvent.click(screen.getByText("Sign Out"));
+    expect(screen.getByText("Authed")).not.toBeUndefined();
   });
 });
--- src/auth/SignOut.tsx
+++ src/auth/SignOut.tsx
@@ -3,12 +3,24 @@
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
+import { Auth } from "firebase/auth";
 import { ReactNode } from "react";
+import { useSignOut } from ".";
 
 type SignOutProps = {
+  auth: Auth;
   onReady: () => ReactNode;
+  onDone?: () => ReactNode;
 };
 
-export const SignOut = ({ onReady }: SignOutProps) => {
-  return onReady();
+export const SignOut = ({ auth, onReady, onDone }: SignOutProps) => {
+  const { state, dispatch } = useSignOut({ auth });
+
+  switch (state) {
+    case "ready":
+      return <div onClick={dispatch}>{onReady()}</div>;
+    case "loading":
+    case "done":
+      return onDone ? onDone() : <></>;
+  }
 };
