e7125fb7ac012c9d8a5c9a317da3254f2e0497b0
Eray Erdin <eraygezer.94@gmail.com>
2024-01-27 16:49:58 +0300

    test when anon, useSignUp hook > should have ready state

--- src/auth/useSignUp.hook.test.tsx
+++ src/auth/useSignUp.hook.test.tsx
@@ -4,9 +4,27 @@
 // https://opensource.org/licenses/MIT
 
 import { renderHook } from "@testing-library/react";
+import { signInAnonymously, signOut } from "firebase/auth";
 import { useSignUp } from ".";
+import { auth } from "../firebase";
 
 describe("when real anon, useSignUp hook", () => {
+  it("should have ready state", async () => {
+    const { result } = renderHook(() => useSignUp());
+    const { state } = result.current;
+    expect(state).toBe("ready");
+  });
+});
+
+describe("when anon, useSignUp hook", () => {
+  beforeEach(async () => {
+    await signInAnonymously(auth);
+  });
+
+  afterEach(async () => {
+    await signOut(auth);
+  });
+
   it("should have ready state", async () => {
     const { result } = renderHook(() => useSignUp());
     const { state } = result.current;
