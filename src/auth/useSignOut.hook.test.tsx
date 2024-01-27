c42b70a6c487faa1292a238d1801316b4acd4d69
Eray Erdin <eraygezer.94@gmail.com>
2024-01-27 10:37:54 +0300

    test and impl when authed, useSignOut hook > should have ready state

--- src/auth/useSignOut.hook.test.tsx
+++ src/auth/useSignOut.hook.test.tsx
@@ -2,3 +2,40 @@
 //
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
+
+import { renderHook } from "@testing-library/react";
+import {
+  UserCredential,
+  createUserWithEmailAndPassword,
+  deleteUser,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { useSignOut } from ".";
+import { auth } from "../firebase";
+
+const email = "usesignout@hook.com" as const;
+const password = "111111";
+
+const SampleComponent = () => {
+  const currentUser = auth.currentUser;
+
+  return <div>{currentUser ? "authed" : "real anon"}</div>;
+};
+
+describe("when authed, useSignOut hook", () => {
+  let credential: UserCredential;
+
+  beforeEach(async () => {
+    credential = await createUserWithEmailAndPassword(auth, email, password);
+    await signInWithEmailAndPassword(auth, email, password);
+  });
+
+  afterAll(async () => {
+    await deleteUser(credential.user);
+  });
+
+  it("should have ready state", () => {
+    const { result } = renderHook(() => useSignOut({ auth }));
+    expect(result.current.state).toBe("ready");
+  });
+});
--- src/auth/useSignOut.ts
+++ src/auth/useSignOut.ts
@@ -3,4 +3,16 @@
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
-export const useSignOut = () => {};
+import { Auth } from "firebase/auth";
+
+type UseSignOutParams = {
+  auth: Auth;
+};
+
+type UseSignOUt = {
+  state: "ready";
+};
+
+export const useSignOut = ({ auth }: UseSignOutParams): UseSignOUt => {
+  return { state: "ready" };
+};
