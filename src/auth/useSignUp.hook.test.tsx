593bb01a67e227b0164d2aea165eceb9fbaff1d5
Eray Erdin <eraygezer.94@gmail.com>
2024-01-27 18:35:02 +0300

    test and impl when anon, useSignUp hook > should have authenticated state

--- src/auth/useSignUp.hook.test.tsx
+++ src/auth/useSignUp.hook.test.tsx
@@ -4,13 +4,20 @@
 // https://opensource.org/licenses/MIT
 
 import { renderHook } from "@testing-library/react";
-import { signInAnonymously, signOut } from "firebase/auth";
+import {
+  UserCredential,
+  createUserWithEmailAndPassword,
+  deleteUser,
+  signInAnonymously,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
 import { useSignUp } from ".";
 import { auth } from "../firebase";
 
 describe("when real anon, useSignUp hook", () => {
   it("should have ready state", async () => {
-    const { result } = renderHook(() => useSignUp());
+    const { result } = renderHook(() => useSignUp({ auth }));
     const { state } = result.current;
     expect(state).toBe("ready");
   });
@@ -25,9 +32,33 @@ describe("when anon, useSignUp hook", () => {
     await signOut(auth);
   });
 
-  it("should have ready state", async () => {
-    const { result } = renderHook(() => useSignUp());
+  it("should have ready state if not only real anon", async () => {
+    const { result } = renderHook(() =>
+      useSignUp({ auth, onlyRealAnon: false }),
+    );
     const { state } = result.current;
     expect(state).toBe("ready");
+  });
+});
+
+describe("when anon, useSignUp hook", () => {
+  const email = "usesignup@hook.com" as const;
+  const password = "111111" as const;
+  let credential: UserCredential;
+
+  beforeEach(async () => {
+    await createUserWithEmailAndPassword(auth, email, password);
+    credential = await signInWithEmailAndPassword(auth, email, password);
+  });
+
+  afterEach(async () => {
+    await signOut(auth);
+    await deleteUser(credential.user);
+  });
+
+  it("should have authenticated state", async () => {
+    const { result } = renderHook(() => useSignUp({ auth }));
+    const { state } = result.current;
+    expect(state).toBe("authenticated");
   });
 });
--- src/auth/useSignUp.tsx
+++ src/auth/useSignUp.tsx
@@ -3,10 +3,40 @@
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
-type SignUpParams = {
-  state: "ready";
+import { Auth } from "firebase/auth";
+import { useEffect, useState } from "react";
+import { useUser } from ".";
+
+type UseSignUpState = "ready" | "authenticated";
+type UseSignUp = {
+  state: UseSignUpState;
 };
 
-export const useSignUp = (): SignUpParams => {
-  return { state: "ready" };
+type UseSignUpParams = {
+  auth: Auth;
+  onlyRealAnon?: boolean;
+};
+
+export const useSignUp = ({
+  auth,
+  onlyRealAnon = true,
+}: UseSignUpParams): UseSignUp => {
+  const [state, setState] = useState<UseSignUpState>("ready");
+  const user = useUser({ auth });
+
+  useEffect(() => {
+    if (onlyRealAnon) {
+      if (user) {
+        setState("authenticated");
+      }
+    } else {
+      if (user) {
+        if (!user.isAnonymous) {
+          setState("authenticated");
+        }
+      }
+    }
+  }, [user, onlyRealAnon]);
+
+  return { state };
 };
