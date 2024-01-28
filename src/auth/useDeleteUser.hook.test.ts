80ab83e393d7eacb0e7eea91200edacc79d62dcc
Eray Erdin <eraygezer.94@gmail.com>
2024-01-28 13:07:21 +0300

    test when anon, useDeleteUser hook > should have anonymous state

--- src/auth/useDeleteUser.hook.test.ts
+++ src/auth/useDeleteUser.hook.test.ts
@@ -4,13 +4,37 @@
 // https://opensource.org/licenses/MIT
 
 import { renderHook } from "@testing-library/react";
-import { signOut } from "firebase/auth";
+import {
+  UserCredential,
+  deleteUser,
+  signInAnonymously,
+  signOut,
+} from "firebase/auth";
 import { useDeleteUser } from ".";
 import { auth } from "../firebase";
 
 describe("when real anon, useDeleteUser hook", () => {
   beforeEach(async () => {
     await signOut(auth);
+  });
+
+  it("should have anonymous state", async () => {
+    const { result } = renderHook(() => useDeleteUser());
+    const { state } = result.current;
+    expect(state).toBe("anonymous");
+  });
+});
+
+describe("when anon, useDeleteUser hook", () => {
+  let credential: UserCredential;
+
+  beforeEach(async () => {
+    credential = await signInAnonymously(auth);
+  });
+
+  afterEach(async () => {
+    await signOut(auth);
+    await deleteUser(credential.user);
   });
 
   it("should have anonymous state", async () => {
