ea74fb269777d93226f5c305c8f32dffb7d92a5b
Eray Erdin (&mut self) <eraygezer.94@gmail.com>
2024-01-30 15:26:40 +0300

    Add `useSendEmailVerification` hook (#40)
    * impl useSendEmailVerification
    
    * init docs for useSendEmailVerification
    
    * write docs for useSendEmailVerification
    
    * refer to useSendEmailVerification hook in useSignIn docs

---
+++ src/auth/useSendEmailVerification.ts
@@ -0,0 +1,48 @@
+// Copyright (c) 2024 Eray Erdin
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+import { ActionCodeSettings, Auth, sendEmailVerification } from "firebase/auth";
+import { useEffect, useState } from "react";
+import { useUser } from ".";
+
+type UseSendEmailVerificationParams = {
+  auth: Auth;
+};
+
+type UseSendEmailVerificationState = "ready" | "loading" | "done" | "anonymous";
+type UseSendEmailVerificationDispatcher = (
+  actionCodeSettings?: ActionCodeSettings,
+) => Promise<void>;
+type UseSendEmailVerification = {
+  state: UseSendEmailVerificationState;
+  dispatch: UseSendEmailVerificationDispatcher;
+};
+
+export const useSendEmailVerification = ({
+  auth,
+}: UseSendEmailVerificationParams): UseSendEmailVerification => {
+  const user = useUser({ auth });
+  const [state, setState] = useState<UseSendEmailVerificationState>("ready");
+
+  useEffect(() => {
+    if (!user) {
+      setState("anonymous");
+      return;
+    }
+  }, [user]);
+
+  const dispatch: UseSendEmailVerificationDispatcher = async (
+    actionCodeSetting,
+  ) => {
+    setState("loading");
+    await sendEmailVerification(
+      user!, // guaranteed user not null on ready
+      actionCodeSetting,
+    );
+    setState("done");
+  };
+
+  return { state, dispatch: state === "ready" ? dispatch : async () => {} };
+};
