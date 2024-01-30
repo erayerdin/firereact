65fdf9285161b829400e51f9434e77af10e6920b
Eray Erdin <eraygezer.94@gmail.com>
2024-01-31 00:34:08 +0300

    test and impl the state of useCallFunction > should be ready initially

--- src/functions/useCallFunction.hook.test.ts
+++ src/functions/useCallFunction.hook.test.ts
@@ -2,3 +2,14 @@
 //
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
+
+import { renderHook } from "@testing-library/react";
+import { useCallFunction } from ".";
+
+describe("the state of useCallFunction", () => {
+  it("should be ready initially", async () => {
+    const { result } = renderHook(() => useCallFunction());
+    const { state } = result.current;
+    expect(state).toBe("ready");
+  });
+});
--- src/functions/useCallFunction.ts
+++ src/functions/useCallFunction.ts
@@ -3,4 +3,10 @@
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
-export const useCallFunction = () => {};
+type UseCallFunction = {
+  state: "ready";
+};
+
+export const useCallFunction = (): UseCallFunction => {
+  return { state: "ready" };
+};
