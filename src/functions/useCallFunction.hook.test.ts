5d99f7f50839c520a2582049c7e06117ded8686e
Eray Erdin <eraygezer.94@gmail.com>
2024-01-31 00:50:34 +0300

    test and impl the state of useCallFunction > should be done when finished

--- src/functions/useCallFunction.hook.test.ts
+++ src/functions/useCallFunction.hook.test.ts
@@ -5,11 +5,22 @@
 
 import { renderHook } from "@testing-library/react";
 import { useCallFunction } from ".";
+import { functions } from "../firebase";
+
+const name = "sampleFunction" as const;
 
 describe("the state of useCallFunction", () => {
   it("should be ready initially", async () => {
-    const { result } = renderHook(() => useCallFunction());
+    const { result } = renderHook(() => useCallFunction({ functions, name }));
     const { state } = result.current;
     expect(state).toBe("ready");
+  });
+
+  it("should be done when finished", async () => {
+    const { result } = renderHook(() => useCallFunction({ functions, name }));
+    const { invoke } = result.current;
+    await invoke();
+    const { state } = result.current;
+    expect(state).toBe("done");
   });
 });
--- src/functions/useCallFunction.ts
+++ src/functions/useCallFunction.ts
@@ -3,10 +3,40 @@
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
-type UseCallFunction = {
-  state: "ready";
+import {
+  Functions,
+  HttpsCallableOptions,
+  HttpsCallableResult,
+  httpsCallable,
+} from "firebase/functions";
+import { useState } from "react";
+
+type UseCallFunctionParams = {
+  functions: Functions;
+  name: string;
+  httpsCallableOptions?: HttpsCallableOptions;
 };
 
-export const useCallFunction = (): UseCallFunction => {
-  return { state: "ready" };
+type UseCallFunctionState = "ready" | "loading" | "done";
+type UseCallFunctionInvoker = () => Promise<HttpsCallableResult<unknown>>;
+type UseCallFunction = {
+  state: UseCallFunctionState;
+  invoke: UseCallFunctionInvoker;
+};
+
+export const useCallFunction = ({
+  functions,
+  name,
+  httpsCallableOptions,
+}: UseCallFunctionParams): UseCallFunction => {
+  const [state, setState] = useState<UseCallFunctionState>("ready");
+
+  const invoke: UseCallFunctionInvoker = async () => {
+    setState("loading");
+    const r = httpsCallable(functions, name, httpsCallableOptions).call({});
+    setState("done");
+    return r;
+  };
+
+  return { state, invoke };
 };
