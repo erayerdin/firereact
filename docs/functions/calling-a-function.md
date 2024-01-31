733c291db49ad6d80535ce4a92c5471182dd79d8
Eray Erdin (&mut self) <eraygezer.94@gmail.com>
2024-01-31 13:19:50 +0300

    Add `useCallFunction` hook (#43)
    * run functions emulator
    
    * add sampleFunction
    
    * init useCallFunction hook
    
    * test and impl the state of useCallFunction > should be ready initially
    
    * add functions instance for testing environment
    
    * test and impl the state of useCallFunction > should be done when finished
    
    * add data param to invoke
    
    * test the return of invoke > should return the value
    
    * init docs for useCallFunction
    
    * write docs for useCallFunction
    
    * add firebase token to workflow
    
    * create and use gcloud auth key
    
    * use envar to write gcloud file
    
    * change order of execution for firebase emulator
    
    * replace emulator run with JarvusInnovations/background-action@v1
    
    * redefine order
    
    * enable log-output-resume
    
    * enable log-output-if
    
    * remove log-output-if and log-output-resume
    
    * multiline run
    
    * fix typo
    
    * revert back to firebase token
    
    * move sleep step into "Run Firebase Emulators" step
    
    * install dependencies for functions

---
+++ docs/functions/calling-a-function.md
@@ -0,0 +1,5 @@
+# Calling A Function
+
+You can use one of the following to call a function in Firebase Functions:
+
+ - [`useCallFunction` hook](../hooks/useCallFunction.md)
--- src/functions/index.ts
+++ src/functions/index.ts
@@ -1,11 +1,6 @@
 // Copyright (c) 2024 Eray Erdin
+//
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
-export * from "./firestore";
-
-export * from "./providers";
-
-export * from "./auth";
-
-export * from "./functions";
+export * from "./useCallFunction";
