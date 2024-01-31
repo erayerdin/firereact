b8b0e73ad8d359b88fddbd0be2ea0a3558a7e689
Eray Erdin <eraygezer.94@gmail.com>
2024-01-31 13:26:57 +0300

    test and impl firebase functions instance should be accessible

--- src/providers/FirebaseFunctionsProvider.prov.test.tsx
+++ src/providers/FirebaseFunctionsProvider.prov.test.tsx
@@ -2,3 +2,23 @@
 //
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
+
+import { render, screen } from "@testing-library/react";
+import { useContext } from "react";
+import { FirebaseFunctionsContext, FirebaseFunctionsProvider } from ".";
+import { functions } from "../firebase";
+
+const SampleComponent = () => {
+  const functions = useContext(FirebaseFunctionsContext);
+  return <div>{functions?.app.name}</div>;
+};
+
+it("firebase functions instance should be accessible", async () => {
+  render(
+    <FirebaseFunctionsProvider functions={functions}>
+      <SampleComponent />
+    </FirebaseFunctionsProvider>,
+  );
+
+  expect(screen.getByText("[DEFAULT]").innerHTML).toBe("[DEFAULT]");
+});
--- src/providers/FirebaseFunctionsProvider.tsx
+++ src/providers/FirebaseFunctionsProvider.tsx
@@ -3,6 +3,25 @@
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
-export const FirebaseFunctionsProvider = () => {
-  return <div>FirebaseFunctionsProvider</div>;
+import { Functions } from "firebase/functions";
+import { createContext } from "react";
+import { NodeComponent } from "../types";
+
+export const FirebaseFunctionsContext = createContext<Functions | undefined>(
+  undefined,
+);
+
+type FirebaseFunctionsProviderProps = {
+  functions: Functions;
+} & NodeComponent;
+
+export const FirebaseFunctionsProvider = ({
+  functions,
+  children,
+}: FirebaseFunctionsProviderProps) => {
+  return (
+    <FirebaseFunctionsContext.Provider value={functions}>
+      {children}
+    </FirebaseFunctionsContext.Provider>
+  );
 };
