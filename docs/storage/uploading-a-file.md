5244b666623028fbe5e090376b1d77a25652acb2
Eray Erdin (&mut self) <eraygezer.94@gmail.com>
2024-02-01 15:53:30 +0300

    Add `useUploadFile` hook (#47)
    * run storage emulator on firebase:emulators:run script
    
    * init useUploadFile
    
    * useUploadFile hook should initially have ready state
    
    * add storage instance
    
    * group test
    
    * allow all read, write in emulator rules
    
    * wip: test and impl useUploadFile hook > should upload file
    
    * sync lock
    
    * add comment to test
    
    * init docs for useUploadFile hook
    
    * add description for listening state
    
    * write docs for useUploadFile
    
    * add metadata prop to dispatch function
    
    * write metadata prop into docs

---
+++ docs/storage/uploading-a-file.md
@@ -0,0 +1,5 @@
+# Uploading a File
+
+To upload file to Firebase Storage, you can use one of the following methods:
+
+ - [`useUploadFile` hook](../hooks/useUploadFile.md)
---
+++ src/storage/useUploadFile.ts
@@ -0,0 +1,41 @@
+// Copyright (c) 2024 Eray Erdin
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+import {
+  StorageReference,
+  UploadMetadata,
+  UploadResult,
+  uploadBytes,
+} from "firebase/storage";
+import { useState } from "react";
+
+type UseUploadFileProps = {
+  reference: StorageReference;
+};
+
+type UseUploadFileState = "ready" | "loading" | "done";
+type UseUploadFileDispatcher = (
+  file: Buffer | File | Blob,
+  metadata?: UploadMetadata,
+) => Promise<UploadResult>;
+type UseUploadFile = {
+  state: UseUploadFileState;
+  dispatch: UseUploadFileDispatcher;
+};
+
+export const useUploadFile = ({
+  reference,
+}: UseUploadFileProps): UseUploadFile => {
+  const [state, setState] = useState<UseUploadFileState>("ready");
+
+  const dispatch: UseUploadFileDispatcher = async (file, metadata) => {
+    setState("loading");
+    const result = await uploadBytes(reference, file, metadata);
+    setState("done");
+    return result;
+  };
+
+  return { state, dispatch };
+};
