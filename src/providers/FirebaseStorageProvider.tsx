fc29cb83f88938247152137e8a8017bd51b2e36d
Eray Erdin <eraygezer.94@gmail.com>
2024-02-01 17:14:12 +0300

    init FirebaseStorageProvider

---
+++ src/providers/FirebaseStorageProvider.tsx
@@ -0,0 +1,8 @@
+// Copyright (c) 2024 Eray Erdin
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+export const FirebaseStorageProvider = () => {
+  return <div>FirebaseStorageProvider</div>;
+};
