42b8fd7dca3324b69ec7fc6ac0ac1c91e8d17281
Eray Erdin <eraygezer.94@gmail.com>
2024-02-01 10:59:43 +0300

    wip: test and impl useUploadFile hook > should upload file

--- src/storage/useUploadFile.test.ts
+++ src/storage/useUploadFile.test.ts
@@ -4,12 +4,30 @@
 // https://opensource.org/licenses/MIT
 
 import { renderHook } from "@testing-library/react";
+import { getDownloadURL, ref } from "firebase/storage";
+import * as fs from "fs";
 import { useUploadFile } from ".";
+import { storage } from "../firebase";
+
+const reference = ref(storage, "files/README.md");
 
 describe("initially, useUploadFile hook", () => {
   it("should have ready state", async () => {
-    const { result } = renderHook(() => useUploadFile());
+    const { result } = renderHook(() => useUploadFile({ reference }));
     const { state } = result.current;
     expect(state).toBe("ready");
+  });
+});
+
+describe("useUploadFile hook", () => {
+  it("should upload file", async () => {
+    const { result } = renderHook(() => useUploadFile({ reference }));
+    const { dispatch } = result.current;
+    const file = fs.readFileSync("README.md");
+    await dispatch(file);
+
+    const url = await getDownloadURL(reference);
+    const res = await fetch(url);
+    expect(res.ok).toBe(true);
   });
 });
--- src/storage/useUploadFile.ts
+++ src/storage/useUploadFile.ts
@@ -3,10 +3,33 @@
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
-type UseUploadFile = {
-  state: "ready";
+import { StorageReference, UploadResult, uploadBytes } from "firebase/storage";
+import { useState } from "react";
+
+type UseUploadFileProps = {
+  reference: StorageReference;
 };
 
-export const useUploadFile = (): UseUploadFile => {
-  return { state: "ready" };
+type UseUploadFileState = "ready" | "loading" | "done";
+type UseUploadFileDispatcher = (
+  file: Buffer | File | Blob,
+) => Promise<UploadResult>;
+type UseUploadFile = {
+  state: UseUploadFileState;
+  dispatch: UseUploadFileDispatcher;
+};
+
+export const useUploadFile = ({
+  reference,
+}: UseUploadFileProps): UseUploadFile => {
+  const [state, setState] = useState<UseUploadFileState>("ready");
+
+  const dispatch: UseUploadFileDispatcher = async (file) => {
+    setState("loading");
+    const result = await uploadBytes(reference, file);
+    setState("done");
+    return result;
+  };
+
+  return { state, dispatch };
 };
