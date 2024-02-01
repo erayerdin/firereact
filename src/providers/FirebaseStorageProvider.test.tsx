907de02f7f9d82820adff666fd9bc5fdf7b5cd9b
Eray Erdin <eraygezer.94@gmail.com>
2024-02-01 17:18:39 +0300

    test and impl storage instance should be accessible

--- src/providers/FirebaseStorageProvider.test.tsx
+++ src/providers/FirebaseStorageProvider.test.tsx
@@ -2,3 +2,23 @@
 //
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
+
+import { render, screen } from "@testing-library/react";
+import { useContext } from "react";
+import { FirebaseStorageContext, FirebaseStorageProvider } from ".";
+import { storage } from "../firebase";
+
+const SampleComponent = () => {
+  const storage = useContext(FirebaseStorageContext);
+  return <div>{storage?.app.name}</div>;
+};
+
+it("storage instance should be accessible", async () => {
+  render(
+    <FirebaseStorageProvider storage={storage}>
+      <SampleComponent />
+    </FirebaseStorageProvider>,
+  );
+
+  expect(screen.getByText("[DEFAULT]")).not.toBeUndefined();
+});
--- src/providers/FirebaseStorageProvider.tsx
+++ src/providers/FirebaseStorageProvider.tsx
@@ -3,6 +3,25 @@
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
-export const FirebaseStorageProvider = () => {
-  return <div>FirebaseStorageProvider</div>;
+import { FirebaseStorage } from "firebase/storage";
+import { createContext } from "react";
+import { NodeComponent } from "../types";
+
+export const FirebaseStorageContext = createContext<
+  FirebaseStorage | undefined
+>(undefined);
+
+type FirebaseStorageProviderProps = {
+  storage: FirebaseStorage;
+} & NodeComponent;
+
+export const FirebaseStorageProvider = ({
+  storage,
+  children,
+}: FirebaseStorageProviderProps) => {
+  return (
+    <FirebaseStorageContext.Provider value={storage}>
+      {children}
+    </FirebaseStorageContext.Provider>
+  );
 };
