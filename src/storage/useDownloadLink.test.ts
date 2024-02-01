fba2386933dec0bffaff7050784a92f46486683d
Eray Erdin <eraygezer.94@gmail.com>
2024-02-02 01:46:59 +0300

    test and impl initially, useDownloadLink hook should have ready state

--- src/storage/useDownloadLink.test.ts
+++ src/storage/useDownloadLink.test.ts
@@ -2,3 +2,16 @@
 //
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
+
+import { renderHook } from "@testing-library/react";
+import { ref } from "firebase/storage";
+import { useDownloadLink } from ".";
+import { storage } from "../firebase";
+
+const reference = ref(storage, "files/README.md");
+
+it("initially, useDownloadLink hook should have ready state", async () => {
+  const { result } = renderHook(() => useDownloadLink({ reference }));
+  const { state } = result.current;
+  expect(state).toBe("ready");
+});
--- src/storage/useDownloadLink.ts
+++ src/storage/useDownloadLink.ts
@@ -3,4 +3,19 @@
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
-export const useDownloadLink = () => {};
+import { StorageReference } from "firebase/storage";
+
+type UseDownloadLinkParams = {
+  reference: StorageReference;
+};
+
+type UseDownloadLinkState = "ready";
+type UseDownloadLink = {
+  state: UseDownloadLinkState;
+};
+
+export const useDownloadLink = ({
+  reference,
+}: UseDownloadLinkParams): UseDownloadLink => {
+  return { state: "ready" };
+};
