2e370a46e516c6bf6bd007215db6e6ab3cc5311e
Eray Erdin <eraygezer.94@gmail.com>
2024-02-03 02:56:26 +0300

    test and impl initially, useDeleteFile hook should have ready state

--- src/storage/useDeleteFile.ts
+++ src/storage/useDeleteFile.ts
@@ -3,4 +3,22 @@
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
-export const useDeleteFile = () => {};
+import { StorageReference } from "firebase/storage";
+import { useState } from "react";
+
+type UseDeleteFileParams = {
+  reference: StorageReference;
+};
+
+type UseDeleteFileState = "ready";
+type UseDeleteFile = {
+  state: UseDeleteFileState;
+};
+
+export const useDeleteFile = ({
+  reference,
+}: UseDeleteFileParams): UseDeleteFile => {
+  const [state, setState] = useState<UseDeleteFileState>("ready");
+
+  return { state };
+};
