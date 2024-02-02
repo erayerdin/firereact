bbfac03022d214bf6483ea37dbf6704ba8fd7410
Eray Erdin (&mut self) <eraygezer.94@gmail.com>
2024-02-02 20:32:33 +0300

    Add `useDownloadBlob` hook (#55)
    * init useDownloadBlob hook
    
    * test and impl initially, useDownloadBlob hook should have ready state
    
    * wip: add dispatch: UseDownloadBlobDispatcher
    
    * wip: add UseDownloadBlobParams
    
    * impl state
    
    * add loading and done states
    
    * add blob state
    
    * wip: separate dispatch
    
    * impl dispatch
    
    * return blob state
    
    * add adhoc storage reference in tests
    
    * add maxDownloadSizeBytes to dispatch
    
    * init docs for useDownloadBlob
    
    * write docs for useDownloadBlob

---
+++ src/storage/useDownloadBlob.test.ts
@@ -0,0 +1,17 @@
+// Copyright (c) 2024 Eray Erdin
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+import { renderHook } from "@testing-library/react";
+import { ref } from "firebase/storage";
+import { useDownloadBlob } from ".";
+import { storage } from "../firebase";
+
+const reference = ref(storage, "files/README.md");
+
+it("initially, useDownloadBlob hook should have ready state", async () => {
+  const { result } = renderHook(() => useDownloadBlob({ reference }));
+  const { state } = result.current;
+  expect(state).toBe("ready");
+});
---
+++ src/storage/useDownloadBlob.ts
@@ -0,0 +1,38 @@
+// Copyright (c) 2024 Eray Erdin
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+import { StorageReference, getBlob } from "firebase/storage";
+import { useState } from "react";
+
+type UseDownloadBlobParams = {
+  reference: StorageReference;
+};
+
+type UseDownloadBlobState = "ready" | "loading" | "done";
+type UseDownloadBlobDispatcher = (
+  maxDownloadSizeBytes?: number,
+) => Promise<Blob>;
+type UseDownloadBlob = {
+  blob: Blob | undefined;
+  state: UseDownloadBlobState;
+  dispatch: UseDownloadBlobDispatcher;
+};
+
+export const useDownloadBlob = ({
+  reference,
+}: UseDownloadBlobParams): UseDownloadBlob => {
+  const [state, setState] = useState<UseDownloadBlobState>("ready");
+  const [blob, setBlob] = useState<Blob | undefined>(undefined);
+
+  const dispatch: UseDownloadBlobDispatcher = async (maxDownloadSizeBytes) => {
+    setState("loading");
+    const blob = await getBlob(reference, maxDownloadSizeBytes);
+    setState("done");
+    setBlob(blob);
+    return blob;
+  };
+
+  return { blob, state, dispatch };
+};
