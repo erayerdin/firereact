bde557ab643fc0f8469119a3f45287e540ce2c9f
Eray Erdin <eraygezer.94@gmail.com>
2024-02-02 23:10:18 +0300

    add UseDownloadBytesParams

--- src/storage/useDownloadBytes.ts
+++ src/storage/useDownloadBytes.ts
@@ -3,14 +3,21 @@
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
+import { StorageReference } from "firebase/storage";
 import { useState } from "react";
+
+type UseDownloadBytesParams = {
+  reference: StorageReference;
+};
 
 type UseDownloadBytesState = "ready";
 type UseDownloadBytes = {
   state: UseDownloadBytesState;
 };
 
-export const useDownloadBytes = (): UseDownloadBytes => {
+export const useDownloadBytes = ({
+  reference,
+}: UseDownloadBytesParams): UseDownloadBytes => {
   const [state, setState] = useState<UseDownloadBytesState>("ready");
 
   return { state };
