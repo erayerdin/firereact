5db270118109a397b1a0a8f7573b7bf2e814622b
Eray Erdin (&mut self) <eraygezer.94@gmail.com>
2024-02-02 18:40:02 +0300

    Add `StorageDownloadLink` (#53)
    * init StorageDownloadLink component
    
    * add StorageDownloadLinkProps
    
    * impl StorageDownloadLink
    
    * init docs for StorageDownloadLink
    
    * fix typo
    
    * write docs for StorageDownloadLink

---
+++ src/storage/StorageDownloadLink.tsx
@@ -0,0 +1,28 @@
+// Copyright (c) 2024 Eray Erdin
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+import { StorageReference } from "firebase/storage";
+import { ReactNode, useEffect } from "react";
+import { useDownloadLink } from ".";
+
+type StorageDownloadLinkProps = {
+  reference: StorageReference;
+  onLoading?: () => ReactNode;
+  onDone: (link: string) => ReactNode;
+};
+
+export const StorageDownloadLink = ({
+  reference,
+  onLoading = () => <></>,
+  onDone,
+}: StorageDownloadLinkProps) => {
+  const { link, state, dispatch } = useDownloadLink({ reference });
+
+  useEffect(() => {
+    dispatch();
+  }, [dispatch]);
+
+  return state === "done" ? onDone(link!) : onLoading();
+};
