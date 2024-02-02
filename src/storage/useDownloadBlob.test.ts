e3879a9683837af0cfef2e323b6c367d484c7f8f
Eray Erdin <eraygezer.94@gmail.com>
2024-02-02 20:14:04 +0300

    add adhoc storage reference in tests

--- src/storage/useDownloadBlob.test.ts
+++ src/storage/useDownloadBlob.test.ts
@@ -4,10 +4,14 @@
 // https://opensource.org/licenses/MIT
 
 import { renderHook } from "@testing-library/react";
+import { ref } from "firebase/storage";
 import { useDownloadBlob } from ".";
+import { storage } from "../firebase";
+
+const reference = ref(storage, "files/README.md");
 
 it("initially, useDownloadBlob hook should have ready state", async () => {
-  const { result } = renderHook(() => useDownloadBlob());
+  const { result } = renderHook(() => useDownloadBlob({ reference }));
   const { state } = result.current;
   expect(state).toBe("ready");
 });
