44bd60e7118a301ddbf7fcf6b0f1aa1962e9ce6a
Eray Erdin <eraygezer.94@gmail.com>
2024-02-03 03:33:31 +0300

    add StorageMetadataProps

--- src/storage/StorageMetadata.tsx
+++ src/storage/StorageMetadata.tsx
@@ -3,6 +3,19 @@
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
-export const StorageMetadata = () => {
+import { FullMetadata, StorageReference } from "firebase/storage";
+import { ReactNode } from "react";
+
+type StorageMetadataProps = {
+  reference: StorageReference;
+  onLoading?: () => ReactNode;
+  onDone: (metadata: FullMetadata) => ReactNode;
+};
+
+export const StorageMetadata = ({
+  reference,
+  onLoading = () => <></>,
+  onDone,
+}: StorageMetadataProps) => {
   return <div>StorageMetadata</div>;
 };
