d47b45976171a3e84b031d2d2e268e5c0321a0f1
Eray Erdin (&mut self) <eraygezer.94@gmail.com>
2024-02-03 03:09:37 +0300

    Add `useDeleteFile` hook (#59)
    * init useDeleteFile
    
    * test and impl initially, useDeleteFile hook should have ready state
    
    * init dispatcher
    
    * add loading and done states
    
    * impl dispatch
    
    * init docs for useDeleteFile
    
    * write docs for useDeleteFile

---
+++ src/storage/useDeleteFile.test.ts
@@ -0,0 +1,17 @@
+// Copyright (c) 2024 Eray Erdin
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+import { renderHook } from "@testing-library/react";
+import { ref } from "firebase/storage";
+import { useDeleteFile } from ".";
+import { storage } from "../firebase";
+
+const reference = ref(storage, "files/README.md");
+
+it("initially, useDeleteFile hook should have ready state", async () => {
+  const { result } = renderHook(() => useDeleteFile({ reference }));
+  const { state } = result.current;
+  expect(state).toBe("ready");
+});
