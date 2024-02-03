564783fe260bd3e60cd65bfff187357563195372
Eray Erdin <eraygezer.94@gmail.com>
2024-02-03 03:14:47 +0300

    test and impl initially, useFileMetadata hook should have ready state

--- src/storage/useFileMetadata.ts
+++ src/storage/useFileMetadata.ts
@@ -3,4 +3,22 @@
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
-export const useFileMetadata = () => {};
+import { StorageReference } from "firebase/storage";
+import { useState } from "react";
+
+type UseFileMetadataParams = {
+  reference: StorageReference;
+};
+
+type UseFileMetadataState = "ready";
+type UseFileMetadata = {
+  state: UseFileMetadataState;
+};
+
+export const useFileMetadata = ({
+  reference,
+}: UseFileMetadataParams): UseFileMetadata => {
+  const [state, setState] = useState<UseFileMetadataState>("ready");
+
+  return { state };
+};
