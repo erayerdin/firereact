232ea3d31cc95cce4d0bffc6343f81ee24b64a5b
Eray Erdin <eraygezer.94@gmail.com>
2024-02-03 03:32:11 +0300

    init StorageMetadata component

---
+++ src/storage/StorageMetadata.tsx
@@ -0,0 +1,8 @@
+// Copyright (c) 2024 Eray Erdin
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+export const StorageMetadata = () => {
+  return <div>StorageMetadata</div>;
+};
