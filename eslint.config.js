f75ee52a1f13c23f0e495de0591da51f89c41d0b
Eray Erdin <eraygezer.94@gmail.com>
2025-10-06 13:26:26 +0300

    replace .eslintrc with eslint.config.js

---
+++ eslint.config.js
@@ -0,0 +1,41 @@
+import js from "@eslint/js";
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+import prettierPlugin from "eslint-plugin-prettier";
+import reactPlugin from "eslint-plugin-react";
+import reactHooksPlugin from "eslint-plugin-react-hooks";
+import storybookPlugin from "eslint-plugin-storybook";
+
+import globals from "globals";
+
+export default [
+  {
+    languageOptions: {
+      parser: tsParser,
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.jest,
+      },
+    },
+
+    plugins: {
+      "@typescript-eslint": tseslint,
+      react: reactPlugin,
+      "react-hooks": reactHooksPlugin,
+      storybook: storybookPlugin,
+      prettier: prettierPlugin,
+    },
+
+    rules: {
+      ...js.configs.recommended.rules,
+      ...reactPlugin.configs.recommended.rules,
+      ...tseslint.configs.recommended.rules,
+      ...tseslint.configs["eslint-recommended"].rules,
+      ...prettierPlugin.configs.recommended.rules,
+      ...reactHooksPlugin.configs.recommended.rules,
+      ...storybookPlugin.configs.recommended.rules,
+      "react/react-in-jsx-scope": "off",
+    },
+  },
+];
